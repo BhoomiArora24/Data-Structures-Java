ed7d8af405609f7b8002d92703a71e773824e61b
NavyaArora24 <aroranavya78@gmail.com>
2026-07-17 16:44:37 +0530

    Basics

---
+++ Basic/Basic1.java
@@ -0,0 +1,9 @@
+
+public class Basic1{
+    public static void main(String[] args){
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
---
+++ Basic/Basic2.java
@@ -0,0 +1,6 @@
+
+public class Basic2 {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
